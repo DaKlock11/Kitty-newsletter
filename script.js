7e8b58dfdb7dcb4fe0b78c6b0d48cc9965b25006
Ben Junya <bjunya@gmail.com>
2020-10-18 11:56:20 -0700

    Add in-element errors to form for firstName field

--- script.js
+++ script.js
@@ -28,49 +28,63 @@ function isValidEmail(input) {
 /** MUST ALWAYS USE "FOR LOOPS" TO BUILD APPROPRIATE FUNCTIONS OF INPUT TEXT*/
 
 function onClick(event) {
-  const target = event.target; document.getElementById('submitButton');
+  const target = event.target;
 
   if (firstNameElement.value.length === 0) {
-    alert('First name value is required');
+    if (!document.getElementById('firstNameError')) {
+      // Create a <p> tag
+      const firstNameError = document.createElement('p');
+
+      // Set the HTML inside the <p> tag to "First name is invalid"
+      firstNameError.innerHTML = 'First name is invalid';
+      firstNameError.id = 'firstNameError';
+      firstNameError.className = 'error';
+
+      // Get the firstName element's parent element
+      const parent = firstNameElement.parentElement;
+
+      // Append the newly created element to the end of the parent's children
+      parent.appendChild(firstNameError);
+    }
+  } else {
+    const errorElement = document.getElementById('firstNameError');
+    if (errorElement) {
+      errorElement.remove();
+    }
   }
 
   if (lastNameElement.value.length === 0) {
     alert('Last name value is required');
   }
-
   const email = emailElement.value;
   if (!isValidEmail(email)) {
     alert('Email is invalid');
   }
-  
   const phonenum = phoneElement.value;
   if (!isValidPhoneNumber(phonenum)) {
     alert('Phone Number is invalid');
-  }
-  if (isValidPhoneNumber(phonenum)) {
+  } else {
     alert('Phone Number is Valid');
   }
 }
 
 
 function isValidPhoneNumber(input) {
-  
   //let number = input.value;
   if (input.length != 10) {
-    alert('Phone Number must be 10 digits only');
     return false;
   }
   let dashSigns = 0;
   for (let i = 0; i < input.length; i++) {
-    
+
     const currentElement = input[i];
-    
+
     if (currentElement === '-') {
       dashSigns += 1;
     }
   }
   return dashSigns === 0 && input[0] != 0;
-  
+
 }
   // Email address needs:
   // a single '@'
