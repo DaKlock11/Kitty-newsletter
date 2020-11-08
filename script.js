775624b95fbc57ea0102ae748c80510d248430cd
Ben Junya <bjunya@gmail.com>
2020-11-08 12:07:58 -0800

    Lesson and reindenting. Refactor to validate func

--- script.js
+++ script.js
@@ -7,174 +7,173 @@ const emailElement = document.getElementById('email');
 
 const phoneElement = document.getElementById('phoneNum');
 /**
- * Checks to see if a string has 1 '@' sign and ends with .com
- * @param input - Email address to check
- * @return boolean
- */
+* Checks to see if a string has 1 '@' sign and ends with .com
+* @param input - Email address to check
+* @return boolean
+*/
 function isValidEmail(input) {
-  // counter variable to see how many '@' signs are in a string
-  let atSigns = 0;
+    // counter variable to see how many '@' signs are in a string
+    let atSigns = 0;
 
-  for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i];
-    if (currentElement === '@') {
-      atSigns += 1;
+    for (let i = 0; i < input.length; i++) {
+        const currentElement = input[i];
+        if (currentElement === '@') {
+            atSigns += 1;
+        }
     }
-  }
 
-  return atSigns === 1 && input.endsWith('.com');
+    return atSigns === 1 && input.endsWith('.com');
+}
+
+
+// DRY - stands for "Don't Repeat Yourself"
+
+function validateField(element, fieldName) {
+
+    let validEmail;
+    if (fieldName === 'email') {
+        validEmail = isValidEmail(element.value);
+    }
+
+
+
+    if (element.value.length === 0 || !validEmail) {
+        if(!document.getElementById(fieldName + 'Error')) {
+            const fieldNameError = document.createElement('p');
+
+            let errorMessage = '';
+
+            // if (fieldName === 'firstName') {
+            //   errorMessage = 'First name is invalid';
+            // } else if (fieldName === 'lastName') {
+            //   errorMessage = 'Last name is invalid';
+            // } else if (fieldName === 'email') {
+            //   errorMessage = 'Email is invalid';
+            // }
+
+            switch (fieldName) {
+                case 'firstName': {
+                    errorMessage = 'First name is invalid';
+                    break;
+                }
+                case 'lastName': {
+                    errorMessage = 'Last name is invalid';
+                    break;
+                }
+                case 'email': {
+                    errorMessage = 'email is invalid';
+                    break;
+                }
+            }
+
+            fieldNameError.innerHTML = errorMessage;
+            fieldNameError.id = fieldName + 'Error';
+            fieldNameError.className = 'error';
+
+            // Get the firstName element's parent element
+            const parent = element.parentElement;
+
+            parent.appendChild(fieldNameError);
+        }
+    } else {
+        const errorElement = document.getElementById(fieldName + 'Error');
+        if (errorElement) {
+            errorElement.remove()
+        }
+    }
 }
 
 /** MUST ALWAYS USE "FOR LOOPS" TO BUILD APPROPRIATE FUNCTIONS OF INPUT TEXT*/
 
 function onClick(event) {
-  const target = event.target;
+    const target = event.target;
 
-  // ASSIGNMENT 10/18/2020
-  // Get the same in-form elements showing error validations
-  // for:
-  // * Email Address field
-  // * Last Name field
-  // * Phone Number field
+    // ASSIGNMENT 10/18/2020
+    // Get the same in-form elements showing error validations
+    // for:
+    // * Email Address field
+    // * Last Name field
+    // * Phone Number field
 
-  // EXTRA CREDIT - Make the errors pretty
+    // EXTRA CREDIT - Make the errors pretty
 
-  // ACCEPTANCE CRITERIA
-  // Errors on fields should persist if the field is not corrent.
-  // Errors on fields should go away if the error is corrected and the "submit" button is clicked
-  // No alert() statements in the final product
-  // No console.log() statements in the final product
-  // No debugger; statements in the final product
+    // ACCEPTANCE CRITERIA
+    // Errors on fields should persist if the field is not corrent.
+    // Errors on fields should go away if the error is corrected and the "submit" button is clicked
+    // No alert() statements in the final product
+    // No console.log() statements in the final product
+    // No debugger; statements in the final product
 
-  if (firstNameElement.value.length === 0) {
-    if (!document.getElementById('firstNameError')) {
-      // Create a <p> tag
-      const firstNameError = document.createElement('p');
+    validateField(firstNameElement, 'firstName');
+    validateField(lastNameElement, 'lastName');
+    validateField(emailElement, 'email');
 
-      // Set the HTML inside the <p> tag to "First name is invalid"
-      firstNameError.innerHTML = 'First name is invalid';
-      firstNameError.id = 'firstNameError';
-      firstNameError.className = 'error';
+    // ASSIGNMENT 11/8/2020
+    // Use the validateField function to get the PhoneNumber field
+    // working just like the 3 previous fields
 
-      // Get the firstName element's parent element
-      const parent = firstNameElement.parentElement;
-
-      // Append the newly created element to the end of the parent's children
-      parent.appendChild(firstNameError);
+    const phonenum = phoneElement.value;
+    if (!isValidPhoneNumber(phonenum)) {
+        alert('Phone Number is invalid');
+    } else {
+        alert('Phone Number is Valid');
     }
-  } else {
-    const errorElement = document.getElementById('firstNameError');
-    if (errorElement) {
-      errorElement.remove();
-    }
-  }
-
-  if (lastNameElement.value.length === 0) {
-    if (!document.getElementById('lastNameError')) {
-      
-      const lastNameError = document.createElement('p');
-      
-      lastNameError.innerHTML = 'Last name is invalid';
-      lastNameError.id = 'lastNameError';
-      lastNameError.classname = 'error';
-      
-      const parent = lastNameElement.parentElement;
-      parent.appendChild(lastNameError);
-    } 
-  } else {
-      const errorElement = document.getElementById('lastNameError');
-      if (errorElement) {
-        errorElement.remove();
-      }
-    }
-    //alert('Last name value is required');
-  
-  if (emailElement.value.length === 0) {
-    if (!document.getElementById('emailError')) {
-      
-      const emailError = document.createElement('p');
-      
-      emailError.innerHTML = 'email is invalid';
-      emailError.id = 'emailError';
-      emailError.classname = 'error';
-      
-      const parent = emailElement.parentElement;
-      parent.appendChild(emailErrorError);
-    } 
-  } else {
-      const errorElement = document.getElementById('emailError');
-      if (errorElement) {
-        errorElement.remove();
-      }
-    }
-    
-  
-  /*const email = emailElement.value;
-  if (!isValidEmail(email)) {
-    alert('Email is invalid');
-  }*/
-  const phonenum = phoneElement.value;
-  if (!isValidPhoneNumber(phonenum)) {
-    alert('Phone Number is invalid');
-  } else {
-    alert('Phone Number is Valid');
-  }
 }
 
 
 function isValidPhoneNumber(input) {
-  //let number = input.value;
-  if (input.length != 10) {
-    return false;
-  }
-  let dashSigns = 0;
-  for (let i = 0; i < input.length; i++) {
-
-    const currentElement = input[i];
-
-    if (currentElement === '-') {
-      dashSigns += 1;
+    //let number = input.value;
+    if (input.length != 10) {
+        return false;
     }
-  }
-  return dashSigns === 0 && input[0] != 0;
+    let dashSigns = 0;
+    for (let i = 0; i < input.length; i++) {
+
+        const currentElement = input[i];
+
+        if (currentElement === '-') {
+            dashSigns += 1;
+        }
+    }
+    return dashSigns === 0 && input[0] != 0;
 
 }
-  // Email address needs:
-  // a single '@'
-  // ends in '.com'
-  // Validate the email address!
+// Email address needs:
+// a single '@'
+// ends in '.com'
+// Validate the email address!
 
-  // VALID:
-  // bjunya@gmail.com
-  // d_klock@live.com
-  // stevie@wonder.com
+// VALID:
+// bjunya@gmail.com
+// d_klock@live.com
+// stevie@wonder.com
 
-  //  INVALID:
-  //  jeff@don@steve.com
-  //  fake@address.net
-  //  tommy@hilfiger.Steve.bannon.sucks@net
-  //  this.is@.a.bad.address.@fish.net.com.net
+//  INVALID:
+//  jeff@don@steve.com
+//  fake@address.net
+//  tommy@hilfiger.Steve.bannon.sucks@net
+//  this.is@.a.bad.address.@fish.net.com.net
 
 
 
-  // NEW ASSIGNMENT 10/4/2020
-  // Add a phone number field
-  // Phone numbers should be strings of 10 characters
-  // (not including country code). Do not include dashes
-  // Phone numbers cannot start with 0
-  // If a phone number includes dashes, throw an error
-  // Phone numbers can only be digits 0-9
+// NEW ASSIGNMENT 10/4/2020
+// Add a phone number field
+// Phone numbers should be strings of 10 characters
+// (not including country code). Do not include dashes
+// Phone numbers cannot start with 0
+// If a phone number includes dashes, throw an error
+// Phone numbers can only be digits 0-9
 
-  // VALID:
-  // 8582312382
-  // 8182340010
-  // 1231231234
+// VALID:
+// 8582312382
+// 8182340010
+// 1231231234
 
-  // INVALID:
-  // 0982801094
-  // 19478109292
-  // 1-232-232-0041
-  // 0898120292
+// INVALID:
+// 0982801094
+// 19478109292
+// 1-232-232-0041
+// 0898120292
 
 
 submitButton.addEventListener('click', onClick);
